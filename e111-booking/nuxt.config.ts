6a9842d37b991802cc4d4eb8c5ff8d05e6dbe35b
colinjen <colinjen88@gmail.com>
2026-02-12 15:18:06 +0800

    Add brand SVG favicon

--- e111-booking/nuxt.config.ts
+++ e111-booking/nuxt.config.ts
@@ -1,6 +1,11 @@
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [{ rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }]
+    }
+  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts', '@vueuse/motion/nuxt'],
